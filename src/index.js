7967784df558d5b76aeb93cd5f0e5786b33c0a0a
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-08-13 08:12:35 +0900

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import _Button from './components/Button';
+import _Container from './components/Container';
+import _Form from './components/Form';
+import _FormLabel from './components/FormLabel';
+import _Navbar from './components/Navbar';
+import _Panel from './components/Panel';
+import _Table from './components/Table';
+import _TextInput from './components/TextInput';
 
 export const Button = _Button;
+export const Container = _Container;
+export const Form = _Form;
+export const FormLabel = _FormLabel;
+export const Navbar = _Navbar;
+export const Panel = _Panel;
+export const Table = _Table;
+export const TextInput = _TextInput;
