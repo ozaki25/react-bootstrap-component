08aee32c7b0b19e4c48420860ce3c0233b3cdb58
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-13 23:10:18 +0900

    update package name

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/my-component.min.js',
+    file: 'dist/index.js',
     format: 'esm',
   },
   plugins: [
