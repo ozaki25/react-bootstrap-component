a3a3ba40ff1dea41d6cc3e9ea4d6f598f965f8ac
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-14 00:33:22 +0900

    modify src/index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import _Button from './components/Button';
 import _Container from './components/Container';
 import _Form from './components/Form';
 import _FormLabel from './components/FormLabel';
-import _Heading from './components/Heading';
+import _Heading, { SubHeading as _SubHeading } from './components/Heading';
 import _Navbar from './components/Navbar';
 import _Panel from './components/Panel';
-import _Table from './components/Table';
+import _Table, {
+  TableHead as _TableHead,
+  TableData as _TableData,
+} from './components/Table';
 import _TextInput from './components/TextInput';
 
 export const Button = _Button;
@@ -13,7 +16,10 @@ export const Container = _Container;
 export const Form = _Form;
 export const FormLabel = _FormLabel;
 export const Heading = _Heading;
+export const SubHeading = _SubHeading;
 export const Navbar = _Navbar;
 export const Panel = _Panel;
 export const Table = _Table;
+export const TableHead = _TableHead;
+export const TableData = _TableData;
 export const TextInput = _TextInput;
